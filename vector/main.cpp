bc0b4049104fb76a667016c812185acc057c07ea
ahbpp <alexeykarpov@MacBook-Air-Alexey.local>
2017-04-03 10:24:03 +0300

    Initial Commit

---
+++ vector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  vector
+//
+//  Created by Alexey Karpov on 03.04.17.
+//  Copyright © 2017 Alexey Karpov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
